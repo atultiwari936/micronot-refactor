95820479ad5595c3cf0faf041603f695d4013750
Vinayak Gaikwad <vinayakg@Vinayaks-MacBook-Pro.local>
2023-01-17 11:21:53 +0530

    Add register model

---
+++ src/main/kotlin/com/tradingplatform/model/Register.kt
@@ -0,0 +1,7 @@
+package com.tradingplatform.model
+
+data class Register(val firstName: String,
+    val lastName: String,
+    val email: String,
+    val userName: String,
+    val phoneNumber: String)
