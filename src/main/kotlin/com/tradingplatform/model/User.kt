25e7b3efd906339381155d22a6260f5a3c7f53ae
AkankshaChaudhari24 <akankshac@Akankshas-MacBook-Pro.local>
2023-01-17 09:41:59 +0530

    add user class

--- src/main/kotlin/com/tradingplatform/model/User.kt
+++ src/main/kotlin/com/tradingplatform/model/User.kt
@@ -1,8 +1,30 @@
 package com.tradingplatform.model
 
-data class User(
-    val firstName: String,
-    val lastName: String,
-    val email: String,
-    val phoneNumber: String,
-    val username: String)
+package ai.sahaj.gurukul.apidesign.model
+
+data class User {
+    val firstName: String
+    val lastName: String
+    val phoneNumber: String
+    val email: String
+    val userName: String
+    val wallet = mapOf<String, Int>()
+    val inventory = mapOf<String, Int>()
+    val orders = arrayOf<Int>()
+    val myList = mutableListOf<Any>()
+
+
+    fun getAccountInfo(): MutableList<Any> {
+        myList.add(firstName)
+        myList.add(lastName)
+        myList.add(phoneNumber)
+        myList.add(email)
+        myList.add(userName)
+        myList.add(wallet)
+        myList.add(inventory)
+        myList.add(orders)
+
+        return myList;
+    }
+
+}
