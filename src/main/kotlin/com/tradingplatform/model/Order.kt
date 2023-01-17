f32aa795dc93530548ed91fe17d83e339ff55f58
MaheshAravindV <maheshv@sahaj.ai>
2023-01-17 10:26:24 +0530

    Add timestamp to each order, move order class to models directory

--- src/main/kotlin/com/tradingplatform/model/Order.kt
+++ src/main/kotlin/com/tradingplatform/model/Order.kt
@@ -1,13 +1,20 @@
-package com.tradingplatform
+package com.tradingplatform.model
 
 data class Order constructor(val type : String, val qty: Int, val price : Int) {
     var status = "unfilled"
     var filled = ArrayList<Pair<Int,Int>>()
     val id = BuyOrders.size + SellOrders.size + CompletedOrders.size
+    val timestamp = System.currentTimeMillis()
     // The match orders function has to be called here
+    init {
+        executeOrders()
+    }
 }
 
+fun executeOrders(){
 
-val BuyOrders = HashMap<Int,Order>()
-val SellOrders = HashMap<Int,Order>()
-val CompletedOrders = HashMap<Int,Order>()
+}
+
+val BuyOrders = HashMap<Int, Order>()
+val SellOrders = HashMap<Int, Order>()
+val CompletedOrders = HashMap<Int, Order>()
