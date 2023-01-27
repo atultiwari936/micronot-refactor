19c1c0dee3870e61ef477d8f301571657f7a2e2b
KCSPreethi <preethik@sahaj.ai>
2023-01-27 16:04:36 +0530

    One SELL order Performance ESOP Test

--- src/test/kotlin/com/tradingplatform/OrderTest.kt
+++ src/test/kotlin/com/tradingplatform/OrderTest.kt
@@ -25,25 +25,36 @@ class OrderTest {
         Users[user1.userName]=user1
         user1.wallet_free=100
 
-
-
         //Act
 
-        var x=OrderController().orderHandler(user1.userName,"BUY",1,20)
-
-
-
+        var x=OrderController().orderHandler(user1.userName,"BUY",1,20,"NORMAL")
 
         //Assert
-
         Assertions.assertEquals(80,user1.wallet_free)
-
-
-
-
-
-
 
     }
 
+
+    @Test
+    fun `Check a single sell order performance`() {
+        //Arrange
+        var user1= User("","","","","kcsp")
+        Users[user1.userName]=user1
+        user1.inventory_free=40
+        user1.perf_free=40
+        user1.wallet_free=100
+
+        //Act
+
+        var x=OrderController().orderHandler(user1.userName,"SELL",10,100,"PERFORMANCE")
+
+        //Assert
+        Assertions.assertEquals(40,user1.inventory_free)
+        Assertions.assertEquals(0,user1.inventory_locked)
+        Assertions.assertEquals(100,user1.wallet_free)
+        Assertions.assertEquals(0,user1.wallet_locked)
+        Assertions.assertEquals(30,user1.perf_free)
+        Assertions.assertEquals(10,user1.perf_locked)
+
+    }
 }
