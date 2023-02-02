8d5f249db35b1899f2a7a41b5bde71be6bd53e97
Pranav-Bhosale <pranavbhosale@sahaj.ai>
2023-02-02 15:52:47 +0530

    branch creation

---
+++ src/test/kotlin/com/tradingplatform/controller/OrderControllerTest.kt
@@ -0,0 +1,12 @@
+package com.tradingplatform.controller
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+@MicronautTest
+class OrderControllerTest
+{
+    @Test
+    fun `check if order`
+}
