060d190e89aeaf874a600d509c841425467eb66d
Atul Kumar Tiwari <atulkumart@sahaj.ai>
2023-02-02 17:21:17 +0530

    Add test case for platform controller

---
+++ src/test/kotlin/com/tradingplatform/controller/PlatformControllerTest.kt
@@ -0,0 +1,29 @@
+package com.tradingplatform.controller
+
+import com.tradingplatform.model.PlatformData
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import io.restassured.specification.RequestSpecification
+import org.hamcrest.Matchers
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.math.BigInteger
+
+@MicronautTest
+class PlatformControllerTest {
+    @BeforeEach
+    fun setUp() {
+        PlatformData.feesEarned = BigInteger("0")
+    }
+
+    @Test
+    fun `should return correct value of collected platform fees`(spec: RequestSpecification) {
+        PlatformData.feesEarned = BigInteger("99")
+
+        spec.`when`()
+            .get("/platform/fees")
+            .then()
+            .statusCode(200).and()
+            .body("collectedFees", Matchers.equalTo(99))
+    }
+}
