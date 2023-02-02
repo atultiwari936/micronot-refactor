dc9c45891b9a70649c015c34e31cbe5b3f0c7dad
KCSPreethi <preethik@sahaj.ai>
2023-02-02 16:19:33 +0530

    fix formatting

--- src/test/kotlin/com/tradingplatform/controller/WalletControllerTest.kt
+++ src/test/kotlin/com/tradingplatform/controller/WalletControllerTest.kt
@@ -6,75 +6,74 @@ import com.tradingplatform.validations.maxLimitForWallet
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import io.restassured.specification.RequestSpecification
 import org.hamcrest.Matchers
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 @MicronautTest
-class WalletControllerTest{
+class WalletControllerTest {
 
     @Test
-    fun `valid amount entered on post request`(spec: RequestSpecification){
+    fun `valid amount entered on post request`(spec: RequestSpecification) {
 
-        val user1= User("","","","tat@gmail.com","sahaj")
-//        UserController().addUser(user1)
+        val user1 = User("", "", "", "tat@gmail.com", "sahaj")
         Users[user1.userName] = user1
         spec.`when`()
-            .header("Content-Type","application/json")
+            .header("Content-Type", "application/json")
             .body("{\"amount\": 10}")
             .post("/user/sahaj/wallet")
             .then()
             .statusCode(200).and()
-            .body("message",Matchers.comparesEqualTo("10 added to account"))
+            .body("message", Matchers.comparesEqualTo("10 added to account"))
 
     }
 
     @Test
-    fun `amount entered is negative`(spec: RequestSpecification){
-        val user1= User("","","","tat@gmail.com","pkcs")
+    fun `amount entered is negative`(spec: RequestSpecification) {
+        val user1 = User("", "", "", "tat@gmail.com", "pkcs")
         UserController().addUser(user1)
         spec.`when`()
-            .header("Content-Type","application/json")
+            .header("Content-Type", "application/json")
             .body("{\"amount\": -10}")
             .post("/user/pkcs/wallet")
             .then()
             .statusCode(400).and()
-            .body("error",Matchers.contains("Enter a positive amount"))
+            .body("error", Matchers.contains("Enter a positive amount"))
 
 
     }
 
     @Test
-    fun `amount entered exceeds maxLimit`(spec: RequestSpecification){
-        val user1= User("","","","tat@gmail.com","pkcs")
+    fun `amount entered exceeds maxLimit`(spec: RequestSpecification) {
+        val user1 = User("", "", "", "tat@gmail.com", "pkcs")
         UserController().addUser(user1)
         spec.`when`()
-            .header("Content-Type","application/json")
+            .header("Content-Type", "application/json")
             .body("{\"amount\": 10000001}")
             .post("/user/pkcs/wallet")
             .then()
             .statusCode(400).and()
-            .body("error",Matchers.contains("Enter amount between 0 to $maxLimitForWallet"))
+            .body("error", Matchers.contains("Enter amount between 0 to $maxLimitForWallet"))
 
 
     }
-    @Test
-    fun `amount entered is a string`(spec:RequestSpecification){
 
-        val user1= User("","","","tat@gmail.com","pkcs")
+    @Test
+    fun `amount entered is a string`(spec: RequestSpecification) {
+
+        val user1 = User("", "", "", "tat@gmail.com", "pkcs")
         UserController().addUser(user1)
         spec.`when`()
-            .header("Content-Type","application/json")
+            .header("Content-Type", "application/json")
             .body("{\"amount\": \"abc\"}")
             .post("/user/pkcs/wallet")
             .then()
             .statusCode(400).and()
-            .body("error",Matchers.contains("Amount data type is invalid"))
+            .body("error", Matchers.contains("Amount data type is invalid"))
 
     }
 
     @Test
-    fun `amount field not entered`(spec:RequestSpecification){
-        val user1= User("","","","tat@gmail.com","pkcs")
+    fun `amount field not entered`(spec: RequestSpecification) {
+        val user1 = User("", "", "", "tat@gmail.com", "pkcs")
         UserController().addUser(user1)
         spec.`when`()
             .header("Content-Type", "application/json")
