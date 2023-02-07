52d4037d69e77c108695a898e1e4c31668df8fd8
Atul Kumar Tiwari <atulkumart@sahaj.ai>
2023-02-07 18:34:22 +0530

    Add WalletReqValidation

--- src/main/kotlin/com/tradingplatform/controller/WalletController.kt
+++ src/main/kotlin/com/tradingplatform/controller/WalletController.kt
@@ -1,18 +1,16 @@
 package com.tradingplatform.controller
 
 import com.tradingplatform.data.UserRepo
-import com.tradingplatform.model.User
-import com.tradingplatform.validations.OrderValidation
 import com.tradingplatform.validations.UserReqValidation
+import com.tradingplatform.validations.WalletReqValidation
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MutableHttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.annotation.Post
+import io.micronaut.json.tree.JsonNode
 import io.micronaut.json.tree.JsonObject
-import kotlin.math.ceil
-import kotlin.math.roundToInt
 
 @Controller(value = "/user")
 class WalletController {
@@ -28,22 +26,12 @@ class WalletController {
             return HttpResponse.badRequest(errorResponse)
         val user = UserRepo.getUser(userName)!!
 
-        val amount = body["amount"]
-        if (amount == null) {
-            response["error"] = errorList
-            errorList.add("Enter the amount field")
-            return HttpResponse.badRequest(response)
-        }
-
-        if (!amount.isNumber || (ceil(amount.doubleValue).roundToInt() != amount.intValue)) {
-            errorList.add("Amount data type is invalid")
-        } else if (OrderValidation().isValidAmount(errorList, body["amount"].intValue))
-            OrderValidation().isWalletAmountWithinLimit(errorList, user, body["amount"].intValue)
-
+        val amount: JsonNode? = body["amount"]
+        errorList.addAll(WalletReqValidation.checkWalletValidations(amount, user))
         response["error"] = errorList
         if (errorList.isNotEmpty()) return HttpResponse.badRequest(response)
 
-        user.wallet.addAmountToFree(amount.intValue)
+        user.wallet.addAmountToFree(amount!!.intValue)
         responseMap["message"] = "${amount.intValue} added to account"
         return HttpResponse.ok(responseMap)
     }
---
+++ src/main/kotlin/com/tradingplatform/validations/WalletReqValidation.kt
@@ -0,0 +1,25 @@
+package com.tradingplatform.validations
+
+import com.tradingplatform.model.User
+import io.micronaut.json.tree.JsonNode
+import kotlin.math.ceil
+import kotlin.math.roundToInt
+
+class WalletReqValidation {
+    companion object {
+        fun checkWalletValidations(amount: JsonNode?, user: User): ArrayList<String> {
+            val errorList = arrayListOf<String>()
+            println("amount value is : $amount")
+            if (amount == null) {
+                errorList.add("Enter the amount field")
+                return errorList
+            }
+
+            if (!amount.isNumber || (ceil(amount.doubleValue).roundToInt() != amount.intValue)) {
+                errorList.add("Amount data type is invalid")
+            } else if (OrderValidation().isValidAmount(errorList, amount.intValue))
+                OrderValidation().isWalletAmountWithinLimit(errorList, user, amount.intValue)
+            return errorList
+        }
+    }
+}
