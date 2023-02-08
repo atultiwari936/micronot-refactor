607f15f626f706edca4cd99e657e4e42ea7af79c
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-08 19:09:46 +0530

    Update error messages

--- src/main/kotlin/com/tradingplatform/dto/OrderRequest.kt
+++ src/main/kotlin/com/tradingplatform/dto/OrderRequest.kt
@@ -8,14 +8,14 @@ import javax.validation.constraints.NotNull
 
 @Introspected
 class OrderRequest @JsonCreator constructor (
-    @field:NotNull(message = "type is required")
-    @field:NotBlank(message = "type is required")
+    @field:NotNull(message = "Enter the quantity field")
+    @field:NotBlank(message = "Enter the quantity field")
     var type: String? = null,
-    @field:NotNull(message = "quantity is required")
-    @field:NotBlank(message = "quantity is required")
+    @field:NotNull(message = "Enter the quantity field")
+    @field:NotBlank(message = "Enter the quantity field")
     var quantity: Int? = null,
-    @field:NotNull(message = "price is required")
-    @field:NotBlank(message = "price is required")
+    @field:NotNull(message = "Enter the quantity field")
+    @field:NotBlank(message = "Enter the quantity field")
     var price: Int? = null,
     var esopType: String? = "NORMAL"
 )
