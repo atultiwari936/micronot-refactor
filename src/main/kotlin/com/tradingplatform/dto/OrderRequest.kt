5512decbeba8db8beaa95df8f5a0383697ff5819
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-09 17:27:58 +0530

    Fix error messages

--- src/main/kotlin/com/tradingplatform/dto/OrderRequest.kt
+++ src/main/kotlin/com/tradingplatform/dto/OrderRequest.kt
@@ -1,21 +1,25 @@
 package com.tradingplatform.dto
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type
 import io.micronaut.core.annotation.Introspected
-import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Min
 import javax.validation.constraints.NotNull
 
 
 @Introspected
-class OrderRequest @JsonCreator constructor (
-    @field:NotNull(message = "Enter the quantity field")
-    @field:NotBlank(message = "Enter the quantity field")
+class OrderRequest @JsonCreator constructor(
+    @field:NotNull(message = "Enter the type field")
     var type: String? = null,
+
     @field:NotNull(message = "Enter the quantity field")
-    @field:NotBlank(message = "Enter the quantity field")
+    @field:Type(Integer::class)
+    @field:Min(1, message = "Order quantity should be greater than 0")
     var quantity: Int? = null,
-    @field:NotNull(message = "Enter the quantity field")
-    @field:NotBlank(message = "Enter the quantity field")
+
+    @field:NotNull(message = "Enter the price field")
+    @field:Min(1, message = "Order price should be greater than 0")
     var price: Int? = null,
+
     var esopType: String? = "NORMAL"
 )
