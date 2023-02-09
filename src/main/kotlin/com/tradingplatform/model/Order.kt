72d8d16eaec9e62a3c488eaacb9fd8bc5b3fe77b
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-09 12:06:44 +0530

    Add default parameter for esop type

--- src/main/kotlin/com/tradingplatform/model/Order.kt
+++ src/main/kotlin/com/tradingplatform/model/Order.kt
@@ -4,7 +4,7 @@ import com.tradingplatform.data.OrderRepository
 
 data class PriceQuantityPair(val price: Int, var quantity: Int) //Utility class to make the response json pretty
 
-data class Order(val type: String, val quantity: Int, val price: Int, val user: User, val esopType: Int) {
+data class Order(val type: String, val quantity: Int, val price: Int, val user: User, val esopType: Int = 0) {
     var status = "unfilled"
     var filled = ArrayList<PriceQuantityPair>()
     val id: Pair<Int, Int> = Pair(
