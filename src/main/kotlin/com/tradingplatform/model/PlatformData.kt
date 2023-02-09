af887f450af66137f3e7f97fa7382cf11fc71416
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-09 22:53:36 +0530

    Update companion object to singleton object

--- src/main/kotlin/com/tradingplatform/model/PlatformData.kt
+++ src/main/kotlin/com/tradingplatform/model/PlatformData.kt
@@ -2,14 +2,13 @@ package com.tradingplatform.model
 
 import java.math.BigInteger
 
-class PlatformData {
-    companion object{
-        val chargeRatio: Double = 0.02
-        var feesEarned: BigInteger = BigInteger("0")
-        val MAX_INVENTORY_LIMIT = 10000000
+object PlatformData {
+    val chargeRatio: Double = 0.02
+    var feesEarned: BigInteger = BigInteger("0")
+    val MAX_INVENTORY_LIMIT = 10000000
 
-        fun calculatePlatformFees(amount: Int): Int {
-            return (amount * chargeRatio).toInt()
-        }
+    fun calculatePlatformFees(amount: Int): Int {
+        return (amount * chargeRatio).toInt()
     }
+
 }
