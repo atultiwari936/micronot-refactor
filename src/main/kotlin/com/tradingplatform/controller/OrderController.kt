1e384946b2e045e9cf70f54f71bae5a00e6f205c
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-02-10 13:49:17 +0530

    Move error handling methods to separate controller

--- src/main/kotlin/com/tradingplatform/controller/OrderController.kt
+++ src/main/kotlin/com/tradingplatform/controller/OrderController.kt
@@ -3,45 +3,22 @@ package com.tradingplatform.controller
 import com.tradingplatform.data.UserRepository
 import com.tradingplatform.dto.OrderRequest
 import com.tradingplatform.dto.OrderResponse
-import com.tradingplatform.exceptions.InvalidOrderException
 import com.tradingplatform.exceptions.UserNotFoundException
 import com.tradingplatform.model.Order
 import com.tradingplatform.model.User
 import com.tradingplatform.services.OrderHistoryService
 import com.tradingplatform.services.OrderService
 import com.tradingplatform.validations.OrderValidation
-import io.micronaut.core.convert.exceptions.ConversionErrorException
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MutableHttpResponse
 import io.micronaut.http.annotation.*
 import io.micronaut.validation.Validated
-import javax.validation.ConstraintViolationException
 import javax.validation.Valid
 
 @Validated
 @Controller(value = "/user")
 class OrderController {
     private val orderService: OrderService = OrderService()
-
-    @Error(exception = ConstraintViolationException::class)
-    fun handleTypeNotPresent(exception: ConstraintViolationException): MutableHttpResponse<Map<String, List<String>>>? {
-        return HttpResponse.badRequest(mapOf("errors" to exception.constraintViolations.map { it.message }))
-    }
-
-    @Error(exception = ConversionErrorException::class)
-    fun handleConversionError(exception: ConversionErrorException): MutableHttpResponse<Map<String, String>>? {
-        return HttpResponse.badRequest(mapOf("errors" to "Invalid data types provided"))
-    }
-
-    @Error(exception = InvalidOrderException::class)
-    fun handleOrderValidationErrors(exception: InvalidOrderException): MutableHttpResponse<Map<String, List<String>>>? {
-        return HttpResponse.badRequest(mapOf("errors" to exception.errors))
-    }
-
-    @Error(exception = UserNotFoundException::class)
-    fun handleUserNotFoundException(exception: UserNotFoundException): MutableHttpResponse<Map<String, List<String>>>? {
-        return HttpResponse.badRequest(mapOf("errors" to exception.errors))
-    }
 
     @Get(value = "/{userName}/order")
     fun orderHistory(@QueryValue userName: String): MutableHttpResponse<MutableList<Order>>? {
